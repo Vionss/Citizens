b1fa466b8d24b49ec06b54346c7114d85bf5e552
Bohdan Havryliv <vions323@gmail.com>
2023-11-15 18:43:46 +0100

    first commit

---
+++ src/ait/citizens/test/CitizensImplTest.java
@@ -0,0 +1,126 @@
+package ait.citizens.test;
+
+import ait.citizens.dao.Citizens;
+import ait.citizens.model.Person;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CitizensImplTest {
+    List<Person> citizens;
+
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        citizens = new LinkedList<>();
+        Person p1 = new Person(1, "F1", "L1", LocalDate.of(1991, 10, 3));
+        Person p2 = new Person(2, "F2", "L2", LocalDate.of(1992, 11, 4));
+        Person p3 = new Person(3, "F3", "L3", LocalDate.of(1993, 12, 5));
+        Person p4 = new Person(4, "F4", "L4", LocalDate.of(1994, 12, 5));
+        Person p5 = new Person(5, "F5", "L5", LocalDate.of(1995, 12, 5));
+        citizens.add(p1);
+        citizens.add(p2);
+        citizens.add(p3);
+        citizens.add(p4);
+        citizens.add(p5);
+    }
+
+    @org.junit.jupiter.api.Test
+    void add() {
+        Person p4 = new Person(4, "F4", "L4", LocalDate.of(1994, 4, 6));
+        assertTrue(citizens.add(p4));
+        assertEquals(6, citizens.size());
+    }
+
+    @org.junit.jupiter.api.Test
+    void remove() {
+        assertTrue(citizens.remove(2));
+        assertEquals(4, citizens.size());
+    }
+
+    @org.junit.jupiter.api.Test
+    void find() {
+        Person p4 = new Person(4, "F4", "L4", LocalDate.of(1994, 4, 6));
+        citizens.add(p4);
+        assertEquals(p4, citizens.find(4));
+    }
+
+    @org.junit.jupiter.api.Test
+    void findMinMaxAge() {
+        citizens = new LinkedList<>();
+        Person p1 = new Person(1, "F1", "L1", LocalDate.of(1991, 10, 3));
+        Person p2 = new Person(2, "F2", "L2", LocalDate.of(1992, 11, 4));
+        Person p3 = new Person(3, "F3", "L3", LocalDate.of(1993, 12, 5));
+        Person p4 = new Person(4, "F4", "L4", LocalDate.of(1994, 12, 5));
+        Person p5 = new Person(5, "F5", "L5", LocalDate.of(1995, 12, 5));
+        citizens.add(p1);
+        citizens.add(p2);
+        citizens.add(p3);
+        List<Person> expected = new LinkedList<>();
+        expected.add(p2);
+        expected.add(p3);
+
+        Iterable<Person> actual = citizens.findMinMaxAge(31, 32);
+        assertIterableEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void findByLastName() {
+        citizens = new LinkedList<>();
+        Person p1 = new Person(1, "F1", "L1", LocalDate.of(1991, 10, 3));
+        Person p2 = new Person(2, "F2", "L2", LocalDate.of(1992, 11, 4));
+        Person p3 = new Person(3, "F3", "L3", LocalDate.of(1993, 12, 5));
+        Person p4 = new Person(4, "F4", "L4", LocalDate.of(1994, 12, 5));
+        Person p5 = new Person(5, "F5", "L5", LocalDate.of(1995, 12, 5));
+        citizens.add(p1);
+        citizens.add(p2);
+        citizens.add(p3);
+        citizens.add(p4);
+        citizens.add(p5);
+        List<Person> expected = new LinkedList<>();
+        expected.add(p3);
+        Iterable<Person> actual = citizens.findByLastname("L3");
+        assertIterableEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAllPersonsById() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAllPersonsByAge() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAllPersonsByLastName() {
+        citizens = new LinkedList<>();
+        Person p1 = new Person(1, "F3", "L1", LocalDate.of(1991, 10, 3));
+        Person p2 = new Person(2, "F1", "L2", LocalDate.of(1992, 11, 4));
+        Person p3 = new Person(3, "F4", "L3", LocalDate.of(1993, 12, 5));
+        Person p4 = new Person(4, "F5", "L4", LocalDate.of(1994, 12, 5));
+        Person p5 = new Person(5, "F2", "L5", LocalDate.of(1995, 12, 5));
+        citizens.add(p1);
+        citizens.add(p2);
+        citizens.add(p3);
+        citizens.add(p4);
+        citizens.add(p5);
+        List<Person> expected = List.of(p2, p5, p1, p3, p4);
+
+        Iterable<Person> actual = List.of(
+                new Person(1, "F3", "L1", LocalDate.of(1991, 10, 3));
+                new Person(2, "F1", "L2", LocalDate.of(1992, 11, 4));
+                new Person(3, "F4", "L3", LocalDate.of(1993, 12, 5));
+                new Person(4, "F5", "L4", LocalDate.of(1994, 12, 5));
+                new Person(5, "F2", "L5", LocalDate.of(1995, 12, 5));)
+        assertIterableEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void size() {
+        assertEquals(5,citizens.size());
+    }
+}
